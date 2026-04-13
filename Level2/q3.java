99afb7066a978fd37c69b88353983a89089fe215
BrendanMoses <brendanmosesjohn@gmail.com>
2026-04-13 08:27:05 +0530

    [Brendan] added: q3 implementation

---
+++ Level2/q3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int[] digits = new int[10];
+        int index = 0;
+        while (num != 0 && index < 10) {
+            digits[index] = num % 10;
+            num = num / 10;
+            index++;
+        }
+        int largest = 0, secondLargest = 0;
+        for (int i = 0; i < index; i++) {
+            if (digits[i] > largest) {
+                secondLargest = largest;
+                largest = digits[i];
+            } else if (digits[i] > secondLargest && digits[i] != largest) {
+                secondLargest = digits[i];
+            }
+        }
+        System.out.println("Largest digit: " + largest);
+        System.out.println("Second largest digit: " + secondLargest);
+    }
+}
